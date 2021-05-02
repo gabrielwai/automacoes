d0dcfd9d56afbceafc67ac394e83dc1a22e4519f
Gabriel Wai Ramos dos Santos <62815808+gabrielwai@users.noreply.github.com>
2021-05-02 15:43:46 -0300

    resolvendo inconsistência de parâmetro (param. link - resetSenhaADP())

--- ResetSenha.py
+++ ResetSenha.py
@@ -1,9 +1,9 @@
 import buscarColaborador
 
 
-def resetSenhaADP(chamadoID, usuario, senha, nomeColaborador=None, emailColaborador=None, cpfColaborador=None):
-    if not buscarColaborador.buscarPorNome(usuario, senha, nomeColaborador):
-        if not buscarColaborador.buscarPorCpf(usuario, senha, cpfColaborador):
+def resetSenhaADP(chamadoID, usuario, senha, link, nomeColaborador=None, emailColaborador=None, cpfColaborador=None):
+    if not buscarColaborador.buscarPorNome(usuario, senha, nomeColaborador, link):
+        if not buscarColaborador.buscarPorCpf(usuario, senha, cpfColaborador, link):
             return False
         else:
             __resetSenha()
